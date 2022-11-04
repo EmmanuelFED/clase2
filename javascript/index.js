05e6bc51c4b647a3f6c800144ffd492fdfb51f9c
EmmanuelFED <em_748@hotmail.com>
2022-11-03 21:06:15 -0300

    0311

--- javascript/index.js
+++ javascript/index.js
@@ -6,3 +6,25 @@ const numeroDos=6;
 
 console.log(nombre)
 console.log(mascota)
+/*
+let a=prompt("ingrese valor")
+
+if (a>1000){
+    alert("es mayor a 1000")
+}
+else {"es menor a 1000"}
+
+let b=prompt("ingrese texto")
+if (b=='hola'){
+    alert("ingresaste hola")
+}
+else {alert("ingresaste algo distinto a hola")}
+
+let c=prompt("ingrese otro numero")
+if(c>10){
+    if (c<50){
+        alert("cumple con el rango 10-50")
+    }
+}
+alert("esta fuera del rango")
+*/
